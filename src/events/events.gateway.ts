6b9bbddee34c91466b30611e523a59d6e4982b4d
Marcos Garcia <marcos_eduardo16@hotmail.com>
2022-07-08 16:08:56 -0400

    this is nice

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,18 +1,33 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer, MessageBody } from '@nestjs/websockets';
-import { Server } from 'socket.io';
-
+import { SubscribeMessage, WebSocketGateway, WebSocketServer, MessageBody, OnGatewayConnection, ConnectedSocket } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { map } from 'rxjs';
 @WebSocketGateway({
   cors: {
     origin: '*'
   }
 })
-export class EventsGateway {
+export class EventsGateway implements OnGatewayConnection {
 
-  @WebSocketServer()
-  server: Server;
+  @WebSocketServer() server: Server;
+  webSockets: any[] = [];
+
+  handleConnection(@ConnectedSocket() client: Socket) {
+    console.log('someone connected:', client.id);
+    this.addClientToArray(client);
+  }
+
+  addClientToArray(client: any) {
+    this.webSockets.push(client);
+    console.log('Total clients connected: ', this.webSockets.length);
+  }
 
   @SubscribeMessage('solicitud-creada')
   handleMessage(@MessageBody() data: any) {
-    return { creada: "true", data: data };
+    this.server.emit('nueva-solicitud');
+  }
+
+  @SubscribeMessage('get-all-clients')
+  sendAllClients() {
+    return this.webSockets;
   }
 }
